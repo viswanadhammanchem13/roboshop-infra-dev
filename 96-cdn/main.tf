5e4a27628dc1c4c575fd841f1cc3edabced0fa02
Manchem <viswanadhammanchem13@gmail.com>
2026-05-18 11:42:53 +0530

    Terraform-Roboshop

--- 96-cdn/main.tf
+++ 96-cdn/main.tf
@@ -5,7 +5,7 @@ resource "aws_cloudfront_distribution" "roboshop" {
         http_port              = 80 // Required to be set but not used
         https_port             = 443
         origin_protocol_policy = "https-only"
-        origin_ssl_protocols   = ["TLSv1.2", "TLSv1.3"]
+        origin_ssl_protocols   = ["TLSv1.2"]
     }
 
     origin_id =  "cdn.${var.zone_name}"
